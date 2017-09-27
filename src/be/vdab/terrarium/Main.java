3013854ab39e7ebdee3e703aa24e659e4cfaa29d
JansenQuinten <quiinten@gmail.com>
2017-09-27 14:15:46 +0200

    TestPush

--- src/be/vdab/terrarium/Main.java
+++ src/be/vdab/terrarium/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello world.");
+		
+		Zever zever = new Zever("Git in eclipse");
+		System.out.println(zever);
 	}
 
 }
--- src/be/vdab/terrarium/Zever.java
+++ src/be/vdab/terrarium/Zever.java
@@ -1,5 +1,16 @@
 package be.vdab.terrarium;
 
 public class Zever {
+	private String gezever;
 
+	public Zever(String gezever) {
+		this.gezever = gezever;
+	}
+
+	public String getGezever() {
+		return gezever;
+	}
+	
+	
+	
 }
