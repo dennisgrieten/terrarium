b7f187b5e6d4351bcb0faf5bfe3e591741708d98
gustvandewouwer <gust.vandewouwer@gmail.com>
2017-10-03 11:37:40 +0200

    commit

--- src/be/vdab/terrarium/model/Terrarium.java
+++ src/be/vdab/terrarium/model/Terrarium.java
@@ -1,13 +1,15 @@
 package be.vdab.terrarium.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public enum Terrarium {
     INSTANCE;
 
-    private final int DIMENSIE = 6;
+    public static final int DIMENSIE = 6;
+    
     private final Random random = new Random();
     private final Organisme[] startOrganismen = {
             new Plant(),
@@ -28,31 +30,109 @@ public enum Terrarium {
 
     Terrarium() {
         matrix = new Cel[DIMENSIE][DIMENSIE];
-        legeCellen = new ArrayList<>();
+        legeCellen = new ArrayList<Cel>();
         initMatrix();
     }
 
+    public Cel[][] getMatrix() {
+    	return matrix;
+    }
+    
+    public int getHoogte() {
+    	return DIMENSIE;
+    }
+    
+    public int getBreedte() {
+    	return DIMENSIE;
+    }
+    
     private void initMatrix() {
-        // initialiseer cellen in de matrix met coÃ¶rdinaten
+    	legeCellen.clear();
+        // initialiseer cellen in de matrix met coördinaten
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = new Cel(i, j);
                 legeCellen.add(matrix[i][j]);
             }
         }
+    }
 
+    public void initStartOrganismen() {
         // vul matrix met start organismen
+    	initMatrix();
         plaatsOrganisme(startOrganismen);
     }
 
+
     // plaats organismen uit een array op random plaatsen in de matrix
-    private void plaatsOrganisme(Organisme[] organismen) {
+    public void plaatsOrganisme(Organisme[] organismen) {
         for (Organisme organisme : organismen) {
             int n = random.nextInt(legeCellen.size());
             legeCellen.get(n).setOrganisme(organisme);
             legeCellen.remove(n);
         }
     }
+    
+    public void plaatsOrganisme(Organisme organisme, int i, int j) {
+    	Cel cel = matrix[i][j];
+    	cel.setOrganisme(organisme);
+    	legeCellen.remove(cel);
+    }
+
+    public void voegNieuwePlantenToe(int aantal) {
+    	Collections.shuffle(legeCellen);
+    	for (int i = 0; i < aantal; i++) {
+    		if (legeCellen.isEmpty()) break;
+    		Cel cel = legeCellen.remove(0);
+    		cel.setOrganisme(new Plant());
+    	}
+    }
+    
+    // voor test
+    public int getAantalPlanten() {
+        int aantal = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                Organisme organisme = matrix[i][j].getOrganisme();
+                if (organisme != null && organisme instanceof Plant) {
+                    aantal++;
+                }
+            }
+        }
+        return aantal;
+    }
+
+    // voor test
+    public int getAantalHerbivoren() {
+        int aantal = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                Organisme organisme = matrix[i][j].getOrganisme();
+                if (organisme != null && organisme instanceof Herbivoor) {
+                    aantal++;
+                }
+            }
+        }
+        return aantal;
+    }
+
+    // voor test
+    public int getAantalCarnivoren() {
+        int aantal = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                Organisme organisme = matrix[i][j].getOrganisme();
+                if (organisme != null && organisme instanceof Carnivoor) {
+                    aantal++;
+                }
+            }
+        }
+        return aantal;
+    }
+
 
     @Override
     public String toString() {
@@ -65,44 +145,46 @@ public enum Terrarium {
         }
         return output.toString();
     }
-    
-//    public void beweeg(int x, int y) {
-//    	if (!beegNaarBovenOK(x, y)) {
-//    		if (!beweegNaarOnderOK) {
-//    			if (!beweegNaarLinksOk) {
-//    				if (!beweegNaarRechtsOK) {
-//    					magicJump();
-//    				}
-//    			}
-//    		}
-//    	}
-//    		
-//    		
-//    }
 
-//	if (((x - 1) < 0) || ((y + 1) > DIMENSIE)) {
-//	
+//  public void beweeg(int x, int y) {
+//	if (!beegNaarBovenOK(x, y)) {
+//		if (!beweegNaarOnderOK) {
+//			if (!beweegNaarLinksOk) {
+//				if (!beweegNaarRechtsOK) {
+//					magicJump();
+//				}
+//			}
+//		}
+//	}
+//		
+//		
+//}
+
+//if (((x - 1) < 0) || ((y + 1) > DIMENSIE)) {
+//
 //}
 //if ((y + 1) > DIMENSIE) {
-//	magicJump();
+//magicJump();
 //} else {
-//	
+//
 //}
 
-    
-    boolean beweegNaarBovenOK(int x, int y) {
-    	if (x - 1 < 0) {
-    		return false;
-    	}
-    	matrix[x - 1][y] = matrix[x][y];
-    	Cel celNaar = (Cel) (matrix[x - 1][y]);
-    	celNaar.setOrganisme(null);
-    	Cel celVan = (Cel) (matrix[x][y]);
-    	celVan.setOrganisme(celNaar.getOrganisme());
-    	return true;
-    }
-    
-    private void magicJump() {
-    	
-    }
-}
+
+	boolean beweegNaarBovenOK(int x, int y) {
+		if (x - 1 < 0) {
+			return false;
+		}
+		matrix[x - 1][y] = matrix[x][y];
+		Cel celNaar = (Cel) (matrix[x - 1][y]);
+		celNaar.setOrganisme(null);
+		Cel celVan = (Cel) (matrix[x][y]);
+		celVan.setOrganisme(celNaar.getOrganisme());
+		return true;
+	}
+	
+	private void magicJump() {
+		
+	}
+
+}    
+
