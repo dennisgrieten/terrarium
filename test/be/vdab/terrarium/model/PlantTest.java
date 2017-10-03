0e023a8ad68fd5ad0caa6ee35bb0908e9f9a2d34
JansenQuinten <quiinten@gmail.com>
2017-10-03 09:14:52 +0200

    Testen voor plant toegevoegd

---
+++ test/be/vdab/terrarium/model/PlantTest.java
@@ -0,0 +1,24 @@
+package be.vdab.terrarium.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlantTest {
+	private Plant plant;
+
+	@Test
+	public void toStringVanPlantIsP() {
+		plant = new Plant();
+		assertEquals("P", plant.toString());
+	}
+	
+	@Test
+	public void levenskrachtVanEenPlantIsEen() {
+		plant = new Plant();
+		assertEquals(1, plant.getLevenskracht());
+	}
+	
+	
+
+}
