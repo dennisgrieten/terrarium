039a0cd7ec85933819725dcbd97ef563429b4e2f
Marc.Smits <Marc.Smits@PC247-15.ad.core.vdabantwerpen.intra>
2017-10-03 10:05:16 +0200

    Plant erft levensKracht gewoon over van Organisme

--- src/be/vdab/terrarium/model/Plant.java
+++ src/be/vdab/terrarium/model/Plant.java
@@ -1,16 +1,7 @@
 package be.vdab.terrarium.model;
 
 public class Plant extends Organisme {
-	private int levensKracht;
-
-	public Plant() {
-		this.levensKracht = 1;
-	}
-
-    public int getLevensKracht() {
-        return levensKracht;
-    }
-
+	
     @Override
 	public String toString() {
 		return "P";
