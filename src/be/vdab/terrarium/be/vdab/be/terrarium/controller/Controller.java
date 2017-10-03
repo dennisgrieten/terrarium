038809f2126c824a3df93dcc77e19c970159923e
Dennis Grieten <dennisgrieten@gmail.com>
2017-10-03 09:23:01 +0200

    Controller toegevoegd

---
+++ src/be/vdab/terrarium/be/vdab/be/terrarium/controller/Controller.java
@@ -0,0 +1,13 @@
+package be.vdab.terrarium.be.vdab.be.terrarium.controller;
+
+import be.vdab.terrarium.model.Terrarium;
+
+public class Controller {
+    public void doeVolgendeIteratie() {
+
+    }
+
+    public Terrarium getTerrarium() {
+        return Terrarium.INSTANCE;
+    }
+}
--- src/be/vdab/terrarium/view/Console.java
+++ src/be/vdab/terrarium/view/Console.java
@@ -1,11 +1,16 @@
 package be.vdab.terrarium.view;
 
+import be.vdab.terrarium.be.vdab.be.terrarium.controller.Controller;
 import be.vdab.terrarium.model.Terrarium;
+
+import javax.naming.ldap.Control;
 
 public class Console {
 
 	public static void main(String[] args) {
-		System.out.println(Terrarium.INSTANCE.toString());
+		Controller controller = new Controller();
+
+		System.out.println(controller.getTerrarium().toString());
 	}
 
 }
