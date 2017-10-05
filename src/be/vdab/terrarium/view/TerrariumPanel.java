25ce8e8c1e8868bd323a4e543a08ab0ab95c8549
marc.smits <mbsmits.vdab@gmail.com>
2017-10-05 10:31:43 +0200

    - aanvang celpanels

--- src/be/vdab/terrarium/view/TerrariumPanel.java
+++ src/be/vdab/terrarium/view/TerrariumPanel.java
@@ -2,70 +2,43 @@ package be.vdab.terrarium.view;
 
 import java.awt.GridLayout;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import be.vdab.terrarium.controller.Controller;
 import be.vdab.terrarium.model.Cel;
-import be.vdab.terrarium.model.Organisme;
 import be.vdab.terrarium.model.Terrarium;
 
 public class TerrariumPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	private static final ImageIcon nietsIcon = new ImageIcon("images/niets.png");
-	private static final ImageIcon plantIcon = new ImageIcon("images/plant.png");
-	private static final ImageIcon herbivoorIcon = new ImageIcon("images/herbivoor.png");
-	private static final ImageIcon carnivoorIcon = new ImageIcon("images/carnivoor.png");
-
 	private int hoogte;
 	private int breedte;
 
-	private JLabel[][] labels;
+	private CelPanel[][] panels;
 
 	public TerrariumPanel() {
 		setBorder(new EmptyBorder(10, 10, 10, 10));
 		Terrarium terrarium = new Controller().getTerrarium();
 		hoogte = terrarium.getHoogte();
 		breedte = terrarium.getBreedte();
-		labels = new JLabel[hoogte][breedte];
+		Cel[][] matrix = terrarium.getMatrix();
+		panels = new CelPanel[hoogte][breedte];
 		setLayout(new GridLayout(hoogte, breedte));
 		for (int y = 0; y < hoogte; y++) {
 			for (int x = 0; x < breedte; x++) {
-				labels[y][x] = new JLabel(nietsIcon);
-				labels[y][x].setBorder(new EmptyBorder(2, 2, 2, 2));
-				add(labels[y][x]);
+				panels[y][x] = new CelPanel(matrix[y][x]);
+				add(panels[y][x]);
 			}
 		}
 		update();
 	}
 
 	public void update() {
-		Terrarium terrarium = new Controller().getTerrarium();
-		Cel[][] matrix = terrarium.getMatrix();
 		for (int y = 0; y < hoogte; y++) {
 			for (int x = 0; x < breedte; x++) {
-				Organisme organisme = matrix[y][x].getOrganisme();
-				if (organisme != null) {
-					switch (organisme.getClass().getSimpleName()) {
-					case "Plant":
-						labels[y][x].setIcon(plantIcon);
-						break;
-					case "Herbivoor":
-						labels[y][x].setIcon(herbivoorIcon);
-						break;
-					case "Carnivoor":
-						labels[y][x].setIcon(carnivoorIcon);
-						break;
-					default:
-						break;
-					}
-				} else {
-					labels[y][x].setIcon(nietsIcon);
-				}
+				panels[y][x].update();
 			}
 		}
 		repaint();
